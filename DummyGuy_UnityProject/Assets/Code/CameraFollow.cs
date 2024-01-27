a0edda236a9b5e8165a3633b4c5cbc8ac98a1dde
Kris Daniel <krisdaniel1452@gmail.com>
2024-01-27 19:56:54 +0200

    Working camera

---
+++ DummyGuy_UnityProject/Assets/Code/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Code
+{
+	public class CameraFollow : MonoBehaviour
+	{
+		[SerializeField] Transform _target;
+		Vector3 _offset;
+
+		void Awake()
+		{
+			_offset = transform.position - _target.position;
+		}
+
+		void LateUpdate()
+		{
+			if(_target)
+				transform.position = Vector3.Lerp(transform.position, _target.position + _offset, Time.deltaTime * 10f);
+		}
+	}
+}
